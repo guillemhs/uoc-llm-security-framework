1d240247aca461940cdc6c59c322ee67e73f1e83
Guillem Hernandez Sola <guillem.hernandez.sola@gmail.com>
2025-11-25 18:19:22 +0100

    Added new environment creation

--- install_venv.sh
+++ install_venv.sh
@@ -13,23 +13,12 @@ python --version
 # Update pip first (good practice)
 pip install --upgrade pip
 
-# Reinstall your libraries
-pip install -r requirements_text.txt
-pip install -r requirements_images.txt
-
-# Uninstall garak if already installed
-pip uninstall -y garak
-
-# Install garak from the main branch without dependencies (they are already installed)
-pip install git+https://github.com/leondz/garak.git@main --no-deps
-
-# Execute one model to test everything is working
-# Set the URL to Hugging Face
-export OPENAI_API_BASE="https://router.huggingface.co/v1"
-
-# Put your HF Token in the OpenAI Key slot
-export OPENAI_API_KEY="hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxx" 
 hf auth login --token YOUR_HUGGINGFACE_API_TOKEN_HERE
-python3 -m garak --model_type huggingface.InferenceAPI --model_name Qwen/Qwen3-0.6B --probes test.Test
 
-python3 -m garak --target_type huggingface --target_name gpt2 --probes test.Test
+export HUGGINGFACE_HUB_TOKEN=YOUR_HUGGINGFACE_API_TOKEN_HERE
+
+# Reinstall your libraries
+pip install -r requirements.txt
+
+# Test the installation
+./batch_test_scan.sh
